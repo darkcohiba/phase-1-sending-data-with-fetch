6d1bcdc9f53f11262b8b3b93166bfcc4fc4c5952
Sam <samuel.waters@gmail.com>
2022-08-12 21:30:40 -0600

    more

--- index.js
+++ index.js
@@ -25,6 +25,25 @@
 //     })
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function submitData( name, email ) {
     return fetch( 'http://localhost:3000/users', {
         method: "POST",
@@ -37,13 +56,52 @@ function submitData( name, email ) {
           email
         } )
       } )
-      .then( function ( response ) {
+      .then(( response) => {
         return response.json()
       } )
-      .then( function ( object ) {
+      .then((object) => {
         document.body.innerHTML = object[ "id" ]
       } )
-      .catch( function ( error ) {
+      .catch((error) => {
         document.body.innerHTML = error.message
       } )
   }
+
+
+
+  // formats our POST
+const postifyObject = (postObject) => {
+
+  return {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+      },
+      body: JSON.stringify(postObject),
+  };
+};
+
+// push response to DOM
+const pushToDOM = (response) => {
+  console.log(response);
+  let newH = document.createElement('h1');
+  newH.innerText = response.id
+  document.querySelector('body').appendChild(newH);
+
+}
+
+// takes the args and makes a call, pushing the 
+const submitData = (inputName, inputEmail) => {
+
+  let postObject = {
+      name: inputName,
+      email: inputEmail,
+    }
+
+return fetch("http://localhost:3000/users", postifyObject(postObject))
+.then(resp => resp.json())
+.then(resp => pushToDOM(resp))
+.catch(error => pushToDOM(error.message));
+
+}
